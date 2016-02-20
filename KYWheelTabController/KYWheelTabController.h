23e0bbe0b95ba8cffb822fb22668ae4bbd02ac7b
ykyouhei <kyouhei.lab@gmail.com>
2016-02-21 01:16:34 +0900

    Initial Commit

---
+++ KYWheelTabController/KYWheelTabController.h
@@ -0,0 +1,19 @@
+//
+//  KYWheelTabController.h
+//  KYWheelTabController
+//
+//  Created by kyo__hei on 2016/02/21.
+//  Copyright © 2016年 kyo__hei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KYWheelTabController.
+FOUNDATION_EXPORT double KYWheelTabControllerVersionNumber;
+
+//! Project version string for KYWheelTabController.
+FOUNDATION_EXPORT const unsigned char KYWheelTabControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KYWheelTabController/PublicHeader.h>
+
+
